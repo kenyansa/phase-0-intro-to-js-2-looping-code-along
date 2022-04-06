f30096ea1bd1b47f54b7aaa8c064c5615301c60b
kenyansa <kenyansajustine@gmail.com>
2022-04-06 19:11:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -3,31 +3,17 @@
 //          returns an array of thank you messages for each name provided to the function:
 const cards =["Guadalupe", "Ollie", "Aki"];
 function writeCards(cards){
-    for (let i = cards.length; i < 0; i--) {
-      console.log(`Thank you, ${cards[i]} for the wonderful surprise gift!`);
+  let x = []
+    for (let i=0; i < cards.length; i++) {
+      x.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
     }
-    return cards;
+    return x;
+}
+function countDown(x){
+  while (x>=0){
+    console.log(x)
+    x--
+  }
 }
 
-
-
-
-//   2) index.js
-//        countDown()
-//          invokes console.log once for each number, counting down from the number provided to zero:
-  
-
-//   3) index.js
-//        countDown()
-//          logs each number when counting down, starting from the number provided:
-// const gifts = ["teddy bear", "drone", "doll"];
-
-// function wrapGifts(gifts) {
-//   for (let i = 0; i < gifts.length; i++) {
-//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
-//     debugger;
-//   }
-
-//   return gifts;
-// }
 
